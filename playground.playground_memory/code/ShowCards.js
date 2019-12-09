0231c8e4c11a31350589ecb1981a840145f678e7
JamieDawson <jamierobertdawson@gmail.com>
2019-12-09 10:01:22 -0800

    sentences now appear at bottom of image_cards

--- playground.playground_memory/code/ShowCards.js
+++ playground.playground_memory/code/ShowCards.js
@@ -7,15 +7,30 @@ module.exports.function = function showCards () {
     "images/9C.png"
   ]
 
+  var sentences = [
+    "Card 1",
+    "Card 2"
+  ]
+
+
   var all = {
-    card_pics: []
+    card_pics: [],
+    card_text: []
   }
+
   
   for (var i = 0; i < cards.length; i++) {
     all.card_pics.push({
-      url: cards[i]
+      url: cards[i],
+      card_text: sentences[i]
     })
   }
+
+  // for (var i = 0; i < cards.length; i++) {
+  //     all.card_text.push({
+  //      card_text: sentences[i]
+  //   })
+  // }
 
 
 
