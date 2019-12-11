0639531df7c5eaee452996d1438614346869f3d5
Jamie Dawson <jadawson@e1z2r7p30.42.us.org>
2019-12-10 17:31:00 -0800

    quiz for 10 cards complete

--- playground.playground_memory/code/ShowCards.js
+++ playground.playground_memory/code/ShowCards.js
@@ -3,13 +3,30 @@ var console = require('console')
 module.exports.function = function showCards () {
 
   var cards = [
-    "images/7H.png",
-    "images/9C.png"
+    "images/7H.png", //1
+    "images/9C.png", //2
+    "images/10C.png", //3
+    "images/JS.png", //4'
+    "images/KD.png", //5
+    "images/AH.png", //6
+    "images/4H.png", //7
+    "images/5D.png", //8
+    "images/QS.png", //9
+    "images/6H.png", //10
+
   ]
 
   var sentences = [
     "Card 1",
-    "Card 2"
+    "Card 2",
+    "Card 3",
+    "Card 4",
+    "Card 5",
+    "Card 6",
+    "Card 7",
+    "Card 8",
+    "Card 9",
+    "Card 10" 
   ]
 
 
